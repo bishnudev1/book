263362e9adea20a02a7bc8e7bf995df35603206d
Bishnudev Khutia <91979889+bishnudev1@users.noreply.github.com>
2024-09-01 08:42:02 -0700

    feat: initial

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:book/splashScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/userModel.dart
@@ -0,0 +1,17 @@
+class UserModel{
+  String profilePhoto;
+  String userName;
+  String destination;
+  String ratingAndReview;
+  String vehicle;
+  String userPrice;
+
+  UserModel({
+    required this.profilePhoto,
+    required this.userName,
+    required this.destination,
+    required this.ratingAndReview,
+    required this.vehicle,
+    required this.userPrice,
+});
+}
